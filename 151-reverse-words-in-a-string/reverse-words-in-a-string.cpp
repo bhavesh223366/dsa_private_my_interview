0325e20bcba3d9153df41747a82b5e5ff040a89c
Bhavesh <143629789+bhavesh223366@users.noreply.github.com>
2026-03-06 20:10:27 -0900

    Time: 5 ms (16.90%) | Memory: 23.6 MB (13.54%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string token;
+        string result = "";
+
+        while (ss >> token) {
+            result = token + " " + result;
+        }
+
+        result.pop_back(); 
+        return result;
+    }
+};
